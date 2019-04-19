1523f11d219b8aa0564c0fab148dd88e08df77cc
sinnkirou.zhao <1210465839@qq.com>
2019-04-20 00:37:02 +0800

    add set-cookie header

--- set-cookie/server.js
+++ set-cookie/server.js
@@ -1,29 +1,20 @@
 const http = require('http');
 const fs = require('fs');
 
-const wait = seconds => new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, seconds * 1000);
-});
-
 http.createServer((request, response) => {
   console.log('request come', request.headers.host);
 
   if (request.url === '/') {
     const html = fs.readFileSync('test.html', 'utf-8');
-    response.writeHead(200, {
-      'Content-Type': 'text/html',
-    });
     response.end(html);
   }
 
   if (request.url === '/data') {
     response.writeHead(200, {
-      'Cache-Control': 'max-age=2, s-maxage=20',
-      Vary: 'User-Agent', // 针对某个头信息相同的情况下才能用缓存，一般会比对User-Agent，Accept等
+      'Content-Type': 'text/html',
+      'Set-Cookie': ['id=123;max-age=2;', 'name=peter;HttpOnly;domain=test.com'],
     });
-    wait(2).then(() => response.end('success'));
+    response.end('success');
   }
 }).listen(3000, () => {
   console.log('server stared on 3000');
