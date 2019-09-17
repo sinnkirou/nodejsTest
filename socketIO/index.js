655a0454154583522eb4b04f6d76a29dd6496edb
caixiang.zhao <caixiang.zhao@shuruitech.com>
2019-09-17 11:24:06 +0800

    update socket io demo

--- socketIO/index.js
+++ socketIO/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const _ = require('lodash');
+
+const hashID = new Array();
 
 server.listen(3000, () => {
   const host = server.address().address;
@@ -37,5 +40,36 @@ io.on('connection', function (socket) {
   socket.on('message', function (data) {
     console.log(data);
     socket.emit('message', data);
+  });
+
+  socket.on('setID', function (id) {
+    hashID[id] = socket.id;
+    console.log(hashID);
+  });
+
+  socket.on('disconnect', function (id) {
+    hashID[id] = null;
+    console.log('user disconnected');
+  });
+
+  socket.on('pMessage', function (toId, data, errorCallBack) {
+    const fromId = hashID.indexOf(socket.id);
+    console.log(
+      'froward a private message from',
+      fromId,
+      ' to ',
+      toId,
+      ' saying ',
+      data.value,
+    );
+    const socketID = hashID[toId];
+    if (socketID) {
+      const toSocket = _.find(io.sockets.sockets, item => item.id === socketID);
+      if (toSocket) {
+        toSocket.emit('message', data);
+      } else {
+        console.log('online message should send');
+      }
+    } else if (errorCallBack) errorCallBack();
   });
 });
